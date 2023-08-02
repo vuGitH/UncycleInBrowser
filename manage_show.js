62c066c763cc26e3d3703faea83e161ae41765ae
Vladimir Uralov <vladimir@uralov.com>
2023-08-02 20:25:07 +0000

    small code improvements of reuid function

--- manage_show.js
+++ manage_show.js
@@ -16,23 +16,20 @@ let startTest = () => {
 /**
  * reconstructs property key oid from uid
  * @param {string} uid universal identifier
- * @param {string=} root level object variable. Default "o"
+ * @param {string=} root upper level object variable. Default "ojo"
  * @return {Array<string>} oids
  */
 let reuid = (uid, root = "o") => { uid.split("#").map((e, i) => { i == 0 ? root : "" + (i > 0 && !(/^\d/.test(e))) ? "." + e : "[" + e + "]" }) };
 reuid = function (uid, root = "o") {
 
-    if (!/^[#]/.test(uid)) {
-        console.log("Bad uid [%s]. First character should be '#'", uid);
-        return [uid];
-    } else if (uid.length === 1) {
+    if (uid === "#") {
         return [root];
     } else if (!/^[#][#].+/.test(uid)) {
         console.log("Incorrect uid format in [%s]", uid);
         return [false];
     } else {
         return uid.slice(1).split("#").map((e, i) => {
-            return i == 0 ? root : ((i > 0 && !(/^\d/.test(e))) ? "." + e : "[" + e + "]");
+            return i == 0 ? root : ((i > 0 && !(/^\d+$/.test(e))) ? "." + e : "[" + e + "]");
         }).join("");
     }     
 };
