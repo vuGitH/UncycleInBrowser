eb0a9fd5c212e8bc1caf6246fb641f3cef9b90c1
Vladimir Uralov <vladimir@uralov.com>
2023-08-05 23:27:36 +0000

    add testBr.js and modify testPage.html by <script>

---
+++ testBr.js
@@ -0,0 +1,17 @@
+let testBr = function(){
+var AAA = "AAA";
+var uc = unCycle;
+//var rev = require('./uncycle').reviver;
+let ojo = ojoTest;
+let oj = JSON.stringify(ojo);
+let ojoPP = uc.postParse(ojo);
+//let reviver = rev;
+let ojoR = JSON.parse(oj,uc.reviverWork);
+console.log(
+    'ojoR.b[2].aa = %j\nojoR.prim.aa=%j',ojoR.b[2].aa, ojoR.prim.aa);
+uc.reviverUser = reviver;
+let ojoRR = JSON.parse(oj,uc.reviver);
+console.log(
+    'ojoRR.b[2].aa = %j\nojoRR.prim.aa =%j',ojoRR.b[2].aa, ojoRR.prim.aa);
+console.log ('Test is close to finish');
+};
