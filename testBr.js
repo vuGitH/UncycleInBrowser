f8578aa2e897d80ef3ba74b72c121e044261021d
Vladimir Uralov <vladimir@uralov.com>
2023-08-06 22:34:35 +0000

    copy of reconnector.js in root dir + small fixes

--- testBr.js
+++ testBr.js
@@ -1,14 +1,16 @@
 let testBr = function(){
 var AAA = "AAA";
 var uc = unCycle;
-//var rev = require('./uncycle').reviver;
+
 let ojo = ojoTest;
 let oj = JSON.stringify(ojo);
 let ojoPP = uc.postParse(ojo);
 //let reviver = rev;
 let ojoR = JSON.parse(oj,uc.reviverWork);
+
 console.log(
     'ojoR.b[2].aa = %j\nojoR.prim.aa=%j',ojoR.b[2].aa, ojoR.prim.aa);
+    
 uc.reviverUser = reviver;
 let ojoRR = JSON.parse(oj,uc.reviver);
 console.log(
